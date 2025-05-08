7e9c7f94efd00d3c1dcbab41a12116c974120ff8
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-08 22:15:44 +0900

    Import PropTypes

--- src/components/CVForm/CVForm.jsx
+++ src/components/CVForm/CVForm.jsx
@@ -1,6 +1,7 @@
 import ContactForm from './ContactForm.jsx';
 import EducationForm from './EducationForm.jsx';
 import ExperienceForm from './ExperienceForm.jsx';
+import PropTypes from 'prop-types';
 
 export default function CVForm({
   contact,
@@ -24,3 +25,36 @@ export default function CVForm({
     </form>
   );
 }
+
+CVForm.propTypes = {
+  contact: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+    city: PropTypes.string,
+    country: PropTypes.string,
+    link: PropTypes.string,
+  }).isRequired,
+  education: PropTypes.arrayOf(
+    PropTypes.shape({
+      universityName: PropTypes.string,
+      city: PropTypes.string,
+      country: PropTypes.string,
+      degree: PropTypes.string,
+      startDate: PropTypes.string,
+      endDate: PropTypes.string,
+    })
+  ).isRequired,
+  experience: PropTypes.arrayOf(
+    PropTypes.shape({
+      jobTitle: PropTypes.string,
+      company: PropTypes.string,
+      startDate: PropTypes.string,
+      endDate: PropTypes.string,
+      description: PropTypes.string,
+    })
+  ).isRequired,
+  onContactChange: PropTypes.func.isRequired,
+  onEducationChange: PropTypes.func.isRequired,
+  onExperienceChange: PropTypes.func.isRequired,
+};
