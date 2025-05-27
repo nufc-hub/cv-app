c3263695501b5368dd464f5ce17531f15f7d399a
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-27 16:17:36 +0900

    Add toggle form sections on button click

--- src/components/CVForm/CVForm.jsx
+++ src/components/CVForm/CVForm.jsx
@@ -8,37 +8,61 @@ export default function CVForm({
   contact,
   education,
   experience,
+  activeForm,
   onContactChange,
   onEducationChange,
   onExperienceChange,
   onAddEducation,
   onAddExperience,
+  onRenderFormSection,
 }) {
   return (
     <>
       <form>
         {/* Button panel */}
         <nav aria-label="Form section navigation">
-          <button type="button">Personal Info</button>
+          <button type="button" onClick={() => onRenderFormSection('contact')}>
+            Personal Info
+          </button>
           <button type="button">Profile</button>
-          <button type="button">Work Experience</button>
-          <button type="button">Education</button>
+          <button
+            type="button"
+            onClick={() => onRenderFormSection('education')}
+          >
+            Education
+          </button>
+          <button
+            type="button"
+            onClick={() => onRenderFormSection('experience')}
+          >
+            Work Experience
+          </button>
           <button type="button">Projects</button>
           <button type="button">Skills</button>
         </nav>
 
         {/* Contact info */}
-        <ContactForm contact={contact} onContactChange={onContactChange} />
-        <EducationForm
-          education={education} // Education array
-          onEducationChange={onEducationChange} // Input element functionality
-          onAddEducation={onAddEducation} // For button click
-        />
-        <ExperienceForm
-          experience={experience} // Experience array
-          onExperienceChange={onExperienceChange} // Input element functionality
-          onAddExperience={onAddExperience} // For button click
-        />
+        {activeForm === 'contact' && (
+          <ContactForm contact={contact} onContactChange={onContactChange} />
+        )}
+
+        {/* Education info */}
+        {activeForm === 'education' && (
+          <EducationForm
+            education={education} // Education array
+            onEducationChange={onEducationChange} // Input element functionality
+            onAddEducation={onAddEducation} // For button click
+          />
+        )}
+
+        {/* Experience info */}
+        {activeForm === 'experience' && (
+          <ExperienceForm
+            experience={experience} // Experience array
+            onExperienceChange={onExperienceChange} // Input element functionality
+            onAddExperience={onAddExperience} // For button click
+          />
+        )}
       </form>
     </>
   );
@@ -72,9 +96,12 @@ CVForm.propTypes = {
       description: PropTypes.string,
     })
   ).isRequired,
+  activeForm: PropTypes.oneOf(['contact', 'education', 'experience'])
+    .isRequired,
   onContactChange: PropTypes.func.isRequired,
   onEducationChange: PropTypes.func.isRequired,
   onExperienceChange: PropTypes.func.isRequired,
   onAddEducation: PropTypes.func.isRequired,
   onAddExperience: PropTypes.func.isRequired,
+  onRenderFormSection: PropTypes.func.isRequired,
 };
