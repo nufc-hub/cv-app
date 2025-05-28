58304c997b65e121e5d3e6a460ecddf23c1eae34
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-28 16:48:17 +0900

    Add values to keys

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -1,26 +1,28 @@
+// Add comments
+
 export const contactLabel = {
-  name: '',
-  email: '',
-  phone: '',
-  city: '',
-  country: '',
-  gitHub: '',
-  linkedIn: '',
+  name: 'Full Name',
+  email: 'Email Address',
+  phone: 'Phone Number',
+  city: 'City',
+  country: 'Country',
+  gitHub: 'GitHub',
+  linkedIn: 'LinkedIn',
 };
 
 export const educationLabel = {
-  universityName: '',
-  city: '',
-  country: '',
-  degree: '',
-  startDate: '',
-  endDate: '',
+  universityName: 'University Name',
+  city: 'City',
+  country: 'Country',
+  degree: 'Degree/Major',
+  startDate: 'Start Date',
+  endDate: 'End Date',
 };
 
 export const experienceLabel = {
-  jobTitle: '',
-  company: '',
-  startDate: '',
-  endDate: '',
-  description: '',
+  jobTitle: 'Job Title',
+  company: 'Company',
+  startDate: 'Start Date',
+  endDate: 'End Date',
+  description: 'Description',
 };
