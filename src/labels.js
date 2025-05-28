d90d63cbc9c4b73bc4ea4788de6bcd599ae8de63
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-28 15:29:44 +0900

    Add form label objects

---
+++ src/labels.js
@@ -0,0 +1,26 @@
+export const contacts = {
+  name: '',
+  email: '',
+  phone: '',
+  city: '',
+  country: '',
+  gitHub: '',
+  linkedIn: '',
+};
+
+export const education = {
+  universityName: '',
+  city: '',
+  country: '',
+  degree: '',
+  startDate: '',
+  endDate: '',
+};
+
+export const experience = {
+  jobTitle: '',
+  company: '',
+  startDate: '',
+  endDate: '',
+  description: '',
+};
