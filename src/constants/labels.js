7e3ce30fdb7324247d8957f6d98e5324e4063800
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-28 23:02:36 +0900

    Add more label objects

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -1,5 +1,4 @@
-// Add comments
-
+// Labels for contact in form
 export const contactLabel = {
   name: 'Full Name',
   email: 'Email Address',
@@ -10,6 +9,12 @@ export const contactLabel = {
   linkedIn: 'LinkedIn',
 };
 
+// Labels for profile in form
+export const profile = {
+  summary: 'Summary',
+};
+
+// Labels for education in form
 export const educationLabel = {
   universityName: 'University Name',
   city: 'City',
@@ -19,10 +24,26 @@ export const educationLabel = {
   endDate: 'End Date',
 };
 
+// Labels for experience in form
 export const experienceLabel = {
   jobTitle: 'Job Title',
   company: 'Company',
   startDate: 'Start Date',
   endDate: 'End Date',
   description: 'Description',
+};
+
+// Labels for project in form
+export const project = {
+  projectName: 'Project Name',
+  role: 'Role',
+  placeOrPlatform: 'Place/Platform',
+  period: 'Period',
+};
+
+// Labels for skill in form
+
+export const skill = {
+  skillCategory: 'Skill Category',
+  skill: 'Skills',
 };
