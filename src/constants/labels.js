c72dd4ee4db1875e81c94f4039c49773318aa5d8
KC <121441283+nufc-hub@users.noreply.github.com>
2025-05-28 15:35:13 +0900

    Move file to constants dir & update variable names

---
+++ src/constants/labels.js
@@ -0,0 +1,26 @@
+export const contactLabel = {
+  name: '',
+  email: '',
+  phone: '',
+  city: '',
+  country: '',
+  gitHub: '',
+  linkedIn: '',
+};
+
+export const educationLabel = {
+  universityName: '',
+  city: '',
+  country: '',
+  degree: '',
+  startDate: '',
+  endDate: '',
+};
+
+export const experienceLabel = {
+  jobTitle: '',
+  company: '',
+  startDate: '',
+  endDate: '',
+  description: '',
+};
