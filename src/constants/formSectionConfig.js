cc6a5f9e3b773d9a6afece164b3bdee99be4d180
KC <121441283+nufc-hub@users.noreply.github.com>
2025-06-03 18:57:51 +0900

    Create a form section map

---
+++ src/constants/formSectionConfig.js
@@ -0,0 +1,101 @@
+import {
+  contactLabel,
+  profileLabel,
+  educationLabel,
+  experienceLabel,
+  projectsLabel,
+  skillsLabel,
+} from './labels';
+
+import ObjectFormSection from '../components/CVForm/ObjectFormSection';
+import ArrayFormSection from '../components/CVForm/ArrayFormSection';
+
+// Renders Each form section
+
+export default function getFormSectionMap(data, handlers) {
+  const { contact, profile, education, experience, projects, skills } = data;
+  const {
+    handleContactInputChange,
+    handleProfileChange,
+    handleEducationChange,
+    handleWorkExperienceChange,
+    handleProjectChange,
+    handleSkillsChange,
+    onAddEducation,
+    onAddExperience,
+    onAddProject,
+    onAddSkills,
+  } = handlers;
+
+  return {
+    // Contact section
+    contact: {
+      Component: ObjectFormSection,
+      props: {
+        title: 'Contact Information', // Section title
+        data: contact, // Contact object
+        labels: contactLabel, //Form labels
+        onInputChange: handleContactInputChange, // This is the input element functionality}
+      },
+    },
+
+    // Profile section
+    profile: {
+      Component: ObjectFormSection,
+      props: {
+        title: 'Profile',
+        data: profile,
+        labels: profileLabel,
+        onInputChange: handleProfileChange,
+      },
+    },
+
+    // Education section
+    education: {
+      Component: ArrayFormSection,
+      props: {
+        title: 'Education',
+        data: education,
+        labels: educationLabel,
+        onInputChange: handleEducationChange,
+        onAddEducation: onAddEducation,
+      },
+    },
+
+    // Work Experience section
+    experience: {
+      Component: ArrayFormSection,
+      props: {
+        title: 'Experience',
+        data: experience,
+        labels: experienceLabel,
+        onInputChange: handleWorkExperienceChange,
+        onAddEducation: onAddExperience,
+      },
+    },
+
+    // Projects section
+    projects: {
+      Component: ArrayFormSection,
+      props: {
+        title: 'Projects',
+        data: projects,
+        labels: projectsLabel,
+        onInputChange: handleProjectChange,
+        onAddEducation: onAddProject,
+      },
+    },
+
+    // Skills section
+    skills: {
+      Component: ArrayFormSection,
+      props: {
+        title: 'Skills',
+        data: skills,
+        labels: skillsLabel,
+        onInputChange: handleSkillsChange,
+        onAddEducation: onAddSkills,
+      },
+    },
+  };
+}
