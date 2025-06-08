2ec6b1c347a78ae05af29c780d2cf0ab5cd76e4c
KC <121441283+nufc-hub@users.noreply.github.com>
2025-06-08 17:19:03 +0900

    Create map

---
+++ src/constants/previewSectionConfig.js
@@ -0,0 +1,77 @@
+import {
+  contactLabel,
+  profileLabel,
+  educationLabel,
+  workExperienceLabel,
+  projectsLabel,
+  skillsLabel,
+} from './labels';
+
+import ObjectPreviewSection from '../components/CVPreview/ObjectPreviewSection';
+import ArrayPreviewSection from '../components/CVPreview/ArrayPreviewSection';
+
+// Used in CV preview section rendering
+export default function getPreviewSectionMap(data) {
+  const { contact, profile, education, workExperience, projects, skills } =
+    data;
+
+  return {
+    // Contact section
+    contact: {
+      Component: ObjectPreviewSection, // Renders the section element
+      props: {
+        data: contact, // Contact object
+        labels: contactLabel, //Form labels
+      },
+    },
+
+    // Profile section
+    profile: {
+      Component: ObjectPreviewSection,
+      props: {
+        data: profile,
+        labels: profileLabel,
+      },
+    },
+
+    // Education section
+    education: {
+      Component: ArrayPreviewSection,
+      props: {
+        title: 'Education',
+        data: education,
+        labels: educationLabel,
+      },
+    },
+
+    // Work Experience section
+    workExperience: {
+      Component: ArrayPreviewSection,
+      props: {
+        title: 'Work Experience',
+        data: workExperience,
+        labels: workExperienceLabel,
+      },
+    },
+
+    // Projects section
+    projects: {
+      Component: ArrayPreviewSection,
+      props: {
+        title: 'Projects',
+        data: projects,
+        labels: projectsLabel,
+      },
+    },
+
+    // Skills section
+    skills: {
+      Component: ArrayPreviewSection,
+      props: {
+        title: 'Skills',
+        data: skills,
+        labels: skillsLabel,
+      },
+    },
+  };
+}
