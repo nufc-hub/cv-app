5a52155cc741aeffd7482971cf1784369a111796
KC <121441283+nufc-hub@users.noreply.github.com>
2025-07-16 22:32:34 +0900

    Import index.css into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'normalize.css';
+import '../src/index.css';
 import App from './components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
